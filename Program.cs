d3d6a8e75657567e6ee2581bed6bb41bba911dc0
Pedro Antonio <68834162+chulastem@users.noreply.github.com>
2023-09-19 07:31:28 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,28 +3,26 @@ namespace Atividades
     public class Program{
         public static void Main(string[] args){
 
-            Console.WriteLine("Lista de Exercicios\nSelecione o exercicio de 1 a 6\n");
-            var result = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Você selecionou {result}º Exercicio\n");
+        Console.WriteLine("Lista de Exercicios\nSelecione o exercicio de 1 a 6\n");
+        var result = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Você selecionou {result}º Exercicio\n");
 
-            switch(result){
-                case 1 : Exercicio_1.Exercise();
-                    break;
-                case 2 : Exercicio_2.Exercise();
-                    break;
-                case 3 : Exercicio_3.Exercise();
-                    break;
-                case 4 : Exercicio_4.Exercise();
-                    break;
-                case 5 : Exercicio_5.Exercise();
-                    break;
-                case 6 : Exercicio_6.Exercise();
-                    break;             
-                default : Console.WriteLine("selecione apenas de 1 a 6: ");
-                    break;
+        switch(result){
+            case 1 : Exercicio_1.Exercise();
+                break;
+            case 2 : Exercicio_2.Exercise();
+                break;
+            case 3 : Exercicio_3.Exercise();
+                break;
+            case 4 : Exercicio_4.Exercise();
+                break;
+            case 5 : Exercicio_5.Exercise();
+                break;
+            case 6 : Exercicio_6.Exercise();
+                break;             
+            default : Console.WriteLine("selecione apenas de 1 a 6: ");
+                break;
             }
-
         }
-
     }
 }
