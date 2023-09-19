921872e18d9769f9070e998ece9376a09b226c08
Pedro Antonio <68834162+chulastem@users.noreply.github.com>
2023-09-19 07:30:14 -0300

    Update Exercicio_1.cs

--- Exercicios/Exercicio_1.cs
+++ Exercicios/Exercicio_1.cs
@@ -3,18 +3,18 @@ public class Exercicio_1
 {
 	public static void Exercise()
 	{
-		int n1, n2, n3, maior = 0, menor = 0;
-		Console.WriteLine("Digite três núneros inteiros: ");
-		n1 = Convert.ToInt32(Console.ReadLine());
-		n2 = Convert.ToInt32(Console.ReadLine());
-		n3 = Convert.ToInt32(Console.ReadLine());
+	int n1, n2, n3, maior = 0, menor = 0;
+	Console.WriteLine("Digite três núneros inteiros: ");
+	n1 = Convert.ToInt32(Console.ReadLine());
+	n2 = Convert.ToInt32(Console.ReadLine());
+	n3 = Convert.ToInt32(Console.ReadLine());
 	
-		maior = (n1 > n2) ? ((n1 > n3) ? n1 : n3) : ((n2 > n3) ? n2 : n3);
-		menor = (n1 < n2) ? ((n1 < n3) ? n1 : n3) : ((n2 < n3) ? n2 : n3);
+	maior = (n1 > n2) ? ((n1 > n3) ? n1 : n3) : ((n2 > n3) ? n2 : n3);
+	menor = (n1 < n2) ? ((n1 < n3) ? n1 : n3) : ((n2 < n3) ? n2 : n3);
 		
-		Console.WriteLine("O maoir número é "+ maior);
-		Console.WriteLine("O menor número é "+ menor);
-		Console.WriteLine("A media aritmetica dos numeros digitados é  "+ (n1+n2+n3)/3);
+	Console.WriteLine("O maoir número é "+ maior);
+	Console.WriteLine("O menor número é "+ menor);
+	Console.WriteLine("A media aritmetica dos numeros digitados é  "+ (n1+n2+n3)/3);
 
         Console.ReadLine();
 	}
